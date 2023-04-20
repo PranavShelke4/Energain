a945626fe4c2da0584f8e985ec43c92caf8a2838
Avadhut-Shedage <avadhutshedage173@gmail.com>
2023-04-20 14:33:53 +0530

    avadhut files

---
+++ Pages/Admin Dashboard/AdminDashboard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dashbord</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="../style/index.css" />
+</head>
+
+<body>
+
+    <div class="flex">
+        <?php
+            require_once('./AdminNavbar.php');
+            require_once('../../components/Admin/Dashboard.php');
+?>
+    </div>
+    
+
+</body>
+
+</html>
